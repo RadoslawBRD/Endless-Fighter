34a56fccd03a26c2020bdfd31c51d941e6068ff2
Radek Brd <radoslaw.brdek@gmail.com>
2022-04-29 10:12:06 +0200

    Rearange files

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -66,4 +66,11 @@ public class Enemy : MonoBehaviour
     {
 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+
+        }
+    }
 }
