06033b2e8402e0893a5ba2a50d0319f619b4bdf9
Radek Brd <radoslaw.brdek@gmail.com>
2022-05-02 15:29:02 +0200

    Hardcoded values to variables + autospawn logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 
@@ -9,6 +10,13 @@ public class GameManager : MonoBehaviour
 
     public GameObject enemyPrefab;
     public Vector2 playerPosition;
+    private Vector2 possibleSpawn;
+    private bool autoSpawn = false;
+    private float tempTime;
+    private float minSpawnDistance = 10f;
+    //game stats///
+    float spawnRate = 1f; //1 sekunda
+
     private void Awake()
     {
         if (instance == null)
@@ -19,21 +27,67 @@ public class GameManager : MonoBehaviour
             Destroy(this);
         }
     }
-        // Start is called before the first frame update
-        void Start()
+    void Start()
     {
         
+       
+        //InvokeRepeating("SpawnEnemy", 0f, spawnRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //druga opcja ciaglego spawnu co minute;
+        if (autoSpawn)
+        {
+            tempTime += Time.deltaTime;
+            if (tempTime > spawnRate)
+            {
+                tempTime = 0f;
+                SpawnEnemy();
+            }
+        }
+
+
+
+
     }
-    public void CreateEnemy()
+    
+
+    public void CreateEnemy(bool _value)
     {
-     
-        Instantiate(enemyPrefab, new Vector2(Random.Range(-20f,20f), Random.Range(-9f, 9f)), Quaternion.identity);
-       // enemies.Add(nextId, new Enemy(new Vector2(2f, 3f)));
+        if(_value)
+            Instantiate(enemyPrefab, new Vector2(Random.Range(-20f,20f), Random.Range(-9f, 9f)), Quaternion.identity);
+        else
+            Instantiate(enemyPrefab, possibleSpawn, Quaternion.identity);
+
+        // enemies.Add(nextId, new Enemy(new Vector2(2f, 3f)));
+    }
+
+    public void SwitchEnemySpawner()
+    {
+        if (autoSpawn)
+            autoSpawn = false;
+        else
+            autoSpawn = true;
+        Debug.Log(autoSpawn);
+    }
+
+
+
+
+    public void SpawnEnemy()
+    {
+        Vector2 _playerpos = PlayerManager.instance.transform.position;
+        Vector2 _possibleSpawn = new Vector2(Random.Range(-20f, 20f), Random.Range(-9f, 9f)); 
+        Debug.Log(_possibleSpawn +" "+ Vector2.Distance(_playerpos, _possibleSpawn));
+        if (Vector2.Distance(_playerpos, _possibleSpawn) > minSpawnDistance) {
+            CreateEnemy(false); //false /true okreœla Ÿród³o respienia przeciwnika
+        }
+        else
+            SpawnEnemy();
+
+
+
     }
 }
